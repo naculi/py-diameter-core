468a7a31db7806630fa6d73091c065dd5d9db040
ganmao@gmail.com <ganmao@gmail.com@cd921824-a84c-246f-e7b0-06b4e6d47db8>
2012-10-10 08:51:08 +0000

    采用os.path.join函数替换原有路径组合方式

--- dev/src/PyDccCore/dcc_config.py
+++ dev/src/PyDccCore/dcc_config.py
@@ -24,7 +24,7 @@ CCR,CCA,...等数组分别保存对应的配置文件信息
     配置文件都需要保存到etc目录下，否则请修改 avp_const_define.ETC_PATH
 '''
 import re
-from os.path import sep
+from os.path import join
 
 import dcc_defined as DCC_DEF
 import dcc_error as DCC_ERR
@@ -62,7 +62,7 @@ class DCC_CFG(object):
         
     def __read_CmdCode(self):
         '读取CmdCode配置文件'
-        fh = open(self.etc_path + sep + "DccCmdCode.ini", "r")
+        fh = open(join(self.etc_path, "DccCmdCode.ini"), "r")
         
         for eachline in fh:
             # 格式化行信息
@@ -82,8 +82,8 @@ class DCC_CFG(object):
         '''根据传入的CmdCodeStr到self.Code2Cmd中确定具体文件
                      之后初始化相应的配置
         '''
-        fh = open(self.etc_path + sep + \
-                  "avpfile-" + CmdCodeStr.lower() + ".ini", "r")
+        fh = open(join(self.etc_path, \
+                  "avpfile-" + CmdCodeStr.lower() + ".ini"), "r")
         
         my_dict = {}
         
